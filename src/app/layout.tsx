113cd8c8aa0cd2052ace1d94e89fddaabda313b4
Sata-P <programhaha@gmail.com>
2024-09-02 23:46:29 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Satakhun Nontha"
+            studentId="660612158"
           />
         </div>
       </body>
